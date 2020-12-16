e2f86cbf9648e47e50dbf0e3e69485b8083f1b0b
Cory Sebastian <sebastiancory@yahoo.com>
2020-12-16 14:23:52 -0500

    added main.py which takes a 32 bit hex number and translates it to binary and chunks it off into mips instruction chunks

---
+++ main.py
@@ -0,0 +1,35 @@
+def main():
+    instruction = input("Enter your instruction in either binary or assembly format: ")
+    decode_instruction(instruction)
+
+
+def decode_instruction(instruction):
+    # if 0 <= int(instruction[0]) <= 1:
+    decode_instr = bin(int(instruction, 16))[2:]
+    print("32 bit instruction")
+    i = 0
+    for bit in range(6):
+        # if i == 6:
+        #     print(decode_instr[0:5] + "\n")
+        # else:
+        # print(bit)
+        if bit == 0:
+            print(decode_instr[bit:bit + 6])
+        elif bit == 1:
+            print(decode_instr[bit+5:11])
+        elif bit == 2:
+            print(decode_instr[bit+9:16])
+        elif bit == 3:
+            print(decode_instr[bit+13:21])
+        elif bit == 4:
+            print(decode_instr[bit + 17:26])
+        else:
+            print(decode_instr[26:32])
+        # print(decode_instr[12:17])
+        # print(decode_instr[18:23])
+        # print(decode_instr[24:29])
+        # print(decode_instr[29:35])
+    print(decode_instr)
+
+
+main()
