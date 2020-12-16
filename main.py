d322276ebf35a35cdf879eb6e419a1ece3466e0d
Cory Sebastian <sebastiancory@yahoo.com>
2020-12-16 14:35:24 -0500

    removed comments

--- main.py
+++ main.py
@@ -4,31 +4,21 @@ def main():
 
 
 def decode_instruction(instruction):
-    # if 0 <= int(instruction[0]) <= 1:
     decode_instr = bin(int(instruction, 16))[2:]
     print("32 bit instruction")
-    i = 0
     for bit in range(6):
-        # if i == 6:
-        #     print(decode_instr[0:5] + "\n")
-        # else:
-        # print(bit)
         if bit == 0:
-            print(decode_instr[bit:bit + 6])
+            print(decode_instr[0:6])
         elif bit == 1:
-            print(decode_instr[bit+5:11])
+            print(decode_instr[6:11])
         elif bit == 2:
-            print(decode_instr[bit+9:16])
+            print(decode_instr[11:16])
         elif bit == 3:
-            print(decode_instr[bit+13:21])
+            print(decode_instr[16:21])
         elif bit == 4:
-            print(decode_instr[bit + 17:26])
+            print(decode_instr[21:26])
         else:
             print(decode_instr[26:32])
-        # print(decode_instr[12:17])
-        # print(decode_instr[18:23])
-        # print(decode_instr[24:29])
-        # print(decode_instr[29:35])
     print(decode_instr)
 
 
